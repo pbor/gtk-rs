4b41caad32a65e2ebe0995fa9e30ff92b1aae5fd
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-12-20 18:14:30 +0100

    glib: add doc alias for manual types

--- glib/src/date.rs
+++ glib/src/date.rs
@@ -26,36 +26,43 @@ unsafe impl Send for Date {}
 unsafe impl Sync for Date {}
 
 impl Date {
+    #[doc(alias = "g_date_new")]
     pub fn new() -> Date {
         unsafe { from_glib_full(ffi::g_date_new()) }
     }
 
+    #[doc(alias = "g_date_new_dmy")]
     pub fn new_dmy(day: DateDay, month: DateMonth, year: DateYear) -> Date {
         unsafe { from_glib_full(ffi::g_date_new_dmy(day, month.to_glib(), year)) }
     }
 
+    #[doc(alias = "g_date_new_julian")]
     pub fn new_julian(julian_day: u32) -> Date {
         unsafe { from_glib_full(ffi::g_date_new_julian(julian_day)) }
     }
 
+    #[doc(alias = "g_date_add_days")]
     pub fn add_days(&mut self, n_days: u32) {
         unsafe {
             ffi::g_date_add_days(self.to_glib_none_mut().0, n_days);
         }
     }
 
+    #[doc(alias = "g_date_add_months")]
     pub fn add_months(&mut self, n_months: u32) {
         unsafe {
             ffi::g_date_add_months(self.to_glib_none_mut().0, n_months);
         }
     }
 
+    #[doc(alias = "g_date_add_years")]
     pub fn add_years(&mut self, n_years: u32) {
         unsafe {
             ffi::g_date_add_years(self.to_glib_none_mut().0, n_years);
         }
     }
 
+    #[doc(alias = "g_date_clamp")]
     pub fn clamp(&mut self, min_date: &Date, max_date: &Date) {
         unsafe {
             ffi::g_date_clamp(
@@ -66,106 +73,128 @@ impl Date {
         }
     }
 
+    #[doc(alias = "g_date_clear")]
     pub fn clear(&mut self, n_dates: u32) {
         unsafe {
             ffi::g_date_clear(self.to_glib_none_mut().0, n_dates);
         }
     }
 
+    #[doc(alias = "g_date_compare")]
     fn compare(&self, rhs: &Date) -> i32 {
         unsafe { ffi::g_date_compare(self.to_glib_none().0, rhs.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_days_between")]
     pub fn days_between(&self, date2: &Date) -> i32 {
         unsafe { ffi::g_date_days_between(self.to_glib_none().0, date2.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_day")]
     pub fn get_day(&self) -> DateDay {
         unsafe { ffi::g_date_get_day(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_day_of_year")]
     pub fn get_day_of_year(&self) -> u32 {
         unsafe { ffi::g_date_get_day_of_year(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_iso8601_week_of_year")]
     pub fn get_iso8601_week_of_year(&self) -> u32 {
         unsafe { ffi::g_date_get_iso8601_week_of_year(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_julian")]
     pub fn get_julian(&self) -> u32 {
         unsafe { ffi::g_date_get_julian(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_monday_week_of_year")]
     pub fn get_monday_week_of_year(&self) -> u32 {
         unsafe { ffi::g_date_get_monday_week_of_year(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_month")]
     pub fn get_month(&self) -> DateMonth {
         unsafe { from_glib(ffi::g_date_get_month(self.to_glib_none().0)) }
     }
 
+    #[doc(alias = "g_date_get_sunday_week_of_year")]
     pub fn get_sunday_week_of_year(&self) -> u32 {
         unsafe { ffi::g_date_get_sunday_week_of_year(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_get_weekday")]
     pub fn get_weekday(&self) -> DateWeekday {
         unsafe { from_glib(ffi::g_date_get_weekday(self.to_glib_none().0)) }
     }
 
+    #[doc(alias = "g_date_get_year")]
     pub fn get_year(&self) -> DateYear {
         unsafe { ffi::g_date_get_year(self.to_glib_none().0) }
     }
 
+    #[doc(alias = "g_date_is_first_of_month")]
     pub fn is_first_of_month(&self) -> bool {
         unsafe { from_glib(ffi::g_date_is_first_of_month(self.to_glib_none().0)) }
     }
 
+    #[doc(alias = "g_date_is_last_of_month")]
     pub fn is_last_of_month(&self) -> bool {
         unsafe { from_glib(ffi::g_date_is_last_of_month(self.to_glib_none().0)) }
     }
 
+    #[doc(alias = "g_date_order")]
     pub fn order(&mut self, date2: &mut Date) {
         unsafe {
             ffi::g_date_order(self.to_glib_none_mut().0, date2.to_glib_none_mut().0);
         }
     }
 
+    #[doc(alias = "g_date_set_day")]
     pub fn set_day(&mut self, day: DateDay) {
         unsafe {
             ffi::g_date_set_day(self.to_glib_none_mut().0, day);
         }
     }
 
+    #[doc(alias = "g_date_set_dmy")]
     pub fn set_dmy(&mut self, day: DateDay, month: DateMonth, y: DateYear) {
         unsafe {
             ffi::g_date_set_dmy(self.to_glib_none_mut().0, day, month.to_glib(), y);
         }
     }
 
+    #[doc(alias = "g_date_set_julian")]
     pub fn set_julian(&mut self, julian_date: u32) {
         unsafe {
             ffi::g_date_set_julian(self.to_glib_none_mut().0, julian_date);
         }
     }
 
+    #[doc(alias = "g_date_set_month")]
     pub fn set_month(&mut self, month: DateMonth) {
         unsafe {
             ffi::g_date_set_month(self.to_glib_none_mut().0, month.to_glib());
         }
     }
 
+    #[doc(alias = "g_date_set_parse")]
     pub fn set_parse(&mut self, str: &str) {
         unsafe {
             ffi::g_date_set_parse(self.to_glib_none_mut().0, str.to_glib_none().0);
         }
     }
 
+    #[doc(alias = "g_date_set_time")]
     pub fn set_time(&mut self, time_: Time) {
         unsafe {
             ffi::g_date_set_time(self.to_glib_none_mut().0, time_);
         }
     }
 
+    #[doc(alias = "g_date_set_time_t")]
     pub fn set_time_t(&mut self, timet: libc::c_long) {
         unsafe {
             ffi::g_date_set_time_t(self.to_glib_none_mut().0, timet);
@@ -176,54 +205,65 @@ impl Date {
     //    unsafe { TODO: call ffi::g_date_set_time_val() }
     //}
 
+    #[doc(alias = "g_date_set_year")]
     pub fn set_year(&mut self, year: DateYear) {
         unsafe {
             ffi::g_date_set_year(self.to_glib_none_mut().0, year);
         }
     }
 
+    #[doc(alias = "g_date_subtract_days")]
     pub fn subtract_days(&mut self, n_days: u32) {
         unsafe {
             ffi::g_date_subtract_days(self.to_glib_none_mut().0, n_days);
         }
     }
 
+    #[doc(alias = "g_date_subtract_months")]
     pub fn subtract_months(&mut self, n_months: u32) {
         unsafe {
             ffi::g_date_subtract_months(self.to_glib_none_mut().0, n_months);
         }
     }
 
+    #[doc(alias = "g_date_subtract_years")]
     pub fn subtract_years(&mut self, n_years: u32) {
         unsafe {
             ffi::g_date_subtract_years(self.to_glib_none_mut().0, n_years);
         }
     }
 
+    //#[doc(alias="g_date_to_struct_tm")]
     //pub fn to_struct_tm(&self, tm: /*Unimplemented*/Fundamental: Pointer) {
     //    unsafe { TODO: call ffi::g_date_to_struct_tm() }
     //}
 
+    #[doc(alias = "g_date_valid")]
     pub fn valid(&self) -> bool {
         unsafe { from_glib(ffi::g_date_valid(self.to_glib_none().0)) }
     }
 
+    #[doc(alias = "g_date_get_days_in_month")]
     pub fn get_days_in_month(month: DateMonth, year: DateYear) -> u8 {
         unsafe { ffi::g_date_get_days_in_month(month.to_glib(), year) }
     }
 
+    #[doc(alias = "g_date_get_monday_weeks_in_year")]
     pub fn get_monday_weeks_in_year(year: DateYear) -> u8 {
         unsafe { ffi::g_date_get_monday_weeks_in_year(year) }
     }
 
+    #[doc(alias = "g_date_get_sunday_weeks_in_year")]
     pub fn get_sunday_weeks_in_year(year: DateYear) -> u8 {
         unsafe { ffi::g_date_get_sunday_weeks_in_year(year) }
     }
 
+    #[doc(alias = "g_date_is_leap_year")]
     pub fn is_leap_year(year: DateYear) -> bool {
         unsafe { from_glib(ffi::g_date_is_leap_year(year)) }
     }
 
+    #[doc(alias = "g_date_strftime")]
     pub fn strftime(s: &str, format: &str, date: &Date) -> usize {
         let slen = s.len() as usize;
         unsafe {
@@ -236,26 +276,32 @@ impl Date {
         }
     }
 
+    #[doc(alias = "g_date_valid_day")]
     pub fn valid_day(day: DateDay) -> bool {
         unsafe { from_glib(ffi::g_date_valid_day(day)) }
     }
 
+    #[doc(alias = "g_date_valid_dmy")]
     pub fn valid_dmy(day: DateDay, month: DateMonth, year: DateYear) -> bool {
         unsafe { from_glib(ffi::g_date_valid_dmy(day, month.to_glib(), year)) }
     }
 
+    #[doc(alias = "g_date_valid_julian")]
     pub fn valid_julian(julian_date: u32) -> bool {
         unsafe { from_glib(ffi::g_date_valid_julian(julian_date)) }
     }
 
+    #[doc(alias = "g_date_valid_month")]
     pub fn valid_month(month: DateMonth) -> bool {
         unsafe { from_glib(ffi::g_date_valid_month(month.to_glib())) }
     }
 
+    #[doc(alias = "g_date_valid_weekday")]
     pub fn valid_weekday(weekday: DateWeekday) -> bool {
         unsafe { from_glib(ffi::g_date_valid_weekday(weekday.to_glib())) }
     }
 
+    #[doc(alias = "g_date_valid_year")]
     pub fn valid_year(year: DateYear) -> bool {
         unsafe { from_glib(ffi::g_date_valid_year(year)) }
     }
